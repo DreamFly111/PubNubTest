68221ce25576d20e66a72cb81fb1c293d9086267
dreamfly <1320027594@qq.com>
2016-08-04 12:44:15 +0800

    必须有注册

--- app/src/main/java/duoy/cn/pubnubtest/Constants.java
+++ app/src/main/java/duoy/cn/pubnubtest/Constants.java
@@ -4,8 +4,11 @@ package duoy.cn.pubnubtest;
  * Created by Dream on 2016/5/26.
  */
 public class Constants {
-    public static final String AVOS_APP_ID = "3UCQMCBl0TT9cygc9pImtm8D-gzGzoHsz";
-    public static final String AVOS_APP_KEY = "xotTQk9eivMGoRPDnGF93KRQ";
+//    public static final String AVOS_APP_ID = "3UCQMCBl0TT9cygc9pImtm8D-gzGzoHsz";
+//    public static final String AVOS_APP_KEY = "xotTQk9eivMGoRPDnGF93KRQ";
+
+    public static final String AVOS_APP_ID = "LWIPB5hUNjDB7AUgdIz9uCXa-gzGzoHsz";
+    public static final String AVOS_APP_KEY = "lm8wu1iiceimKl1KBtmbXKuq";
 
     public static final String PN_PUB_KEY = "pub-c-21bf7caa-f4f6-434c-9f24-a56b4f297ecd"; // MePlusClient PUBKey
     public static final String PN_SUB_KEY = "sub-c-dbbf4102-e02c-11e5-86ad-0619f8945a4f"; // MePlusClient SUBkey
---
+++ app/src/main/java/duoy/cn/pubnubtest/RegisterActivity.java
@@ -0,0 +1,40 @@
+package duoy.cn.pubnubtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.SignUpCallback;
+
+/**
+ * 注册页面
+ */
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        User user = new User();
+        user.setUsername("hjiang" + SystemClock.currentThreadTimeMillis());
+        user.setPassword("123456");
+        user.setEmail("hang" + SystemClock.currentThreadTimeMillis() + "@leancloud.rocks");
+        // 其他属性可以像其他AVObject对象一样使用put方法添加
+        user.put("phone", "186-4321-0000");
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(AVException e) {
+                if (e == null) {
+                    // successfully
+                    finish();
+                } else {
+                    // failed
+                }
+            }
+        });
+
+    }
+
+}
